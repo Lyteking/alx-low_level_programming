bd1e06b19b486da2d6537487a52744801ed42c21
light <lightebube751@gmail.com>
2022-09-11 06:03:09 -0700

    c language

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * return - Return 0 (Success)
+ *
+ */
+int main(void)
+{
+	char LowercaseAlphabets = 'a'; LowercaseAlphabets <= 'z';
+
+	putchar (LowercaseAlphabets);
+	return (0);
+}
