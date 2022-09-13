d9be9c47da80e6450861dc754b13c12cbd9e177c
Lyteking <111017372+Lyteking@users.noreply.github.com>
2022-09-13 15:32:27 -0600

    Create 1-alphabet.c
    c language

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "0-putchar"
+
+/**
+ * print_alphabet - prints lowercase alphabet
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
